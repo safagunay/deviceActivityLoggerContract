0b0eb56338fbe9bcd0f24acca410f490dff996e2
safa19 <safagunay96@gmail.com>
2019-10-24 20:10:15 +0300

    deploy to rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,15 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+
 module.exports = {
   networks: {
     development: {
-      host: "40.114.250.140",
-      port: 8545,
-      network_id: "*" // Match any network id
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, process.env.REMOTE_NODE),
+      network_id: 4
     }
   },
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 200
+
+  mocha: {
+    // timeout: 100000
+  },
+  // Configure your compilers
+  compilers: {
+    solc: {
     }
   }
 }
